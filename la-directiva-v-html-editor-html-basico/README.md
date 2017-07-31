e5ff51ba6a63f1102a4ed2a0f6a1d23b6192499b
calirojas <juancarlosr@msn.com>
2017-07-31 06:24:40 -0500

    Ejemplo de la directiva v-html

---
+++ la-directiva-v-html-editor-html-basico/README.md
@@ -0,0 +1,8 @@
+# Vue.js 2
+## Editor HTML básico con la directiva v-html
+
+Uso de la directiva v-html para renderizar texto en
+formato HTML, en vez de plano.
+
+- Directiva v-html
+- Modelos
---
+++ la-directiva-v-html-editor-html-basico/js/app.js
@@ -0,0 +1,11 @@
+new Vue({
+	el: '#app',
+	data: {
+		codigo: [
+			'<h1>Hola mundo</h1>',
+			'<div class="alert alert-success">',
+			'Editor b&aacute;sico con Vue.js',
+			'</div>',
+			'<a href="http://calirojas.com">calirojas.com</a>'].join('')
+	}
+});
